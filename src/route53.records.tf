8516dafa27ad634497d926491653bfd222298a7e
Nathan Hall <nathan@whatshark.com>
2022-10-06 21:59:08 +0100

    Include google workspace security

--- src/route53.records.tf
+++ src/route53.records.tf
@@ -41,7 +41,35 @@ resource "aws_route53_record" "google-workspace" {
     "5 alt1.aspmx.l.google.com",
     "5 alt2.aspmx.l.google.com",
     "10 alt3.aspmx.l.google.com",
-    "10 alt4.aspmx.l.google.com",
+    "10 alt4.aspmx.l.google.com"
   ]
   ttl = 3600
+}
+
+resource "aws_route53_record" "google-workspace-dkim" {
+  zone_id = aws_route53_zone.main.zone_id
+  name    = "google._domainkey"
+  type    = "TXT"
+  records = [
+    "v=DKIM1; k=rsa; p=MIIBIjANBgkqhkiG9w0BAQEFAAOCAQ8AMIIBCgKCAQEAuBmkxFlQ88PaGUtIL+EIxIjzYQhwm+bZdSsg3rkLNcNMdK9k4O3W3Mc7esPnJMwAJfT69nRPqoxdf9Ue4ZcKQ+RCAn2m8ZkTNVW80PeYcS0SXcEegOOC/2/O4fdaxTqE9B7KKNDLBkIXYI00pctQmQD3/GoZcIRYtldCZynP3/iTDAoSzOMfvvEWPCCxAktrN",
+    "TgmxvK1tzM4LQv/mHkjeMr+Er2tmb3o068dgFzDLvc1hx4I4r3YWeejcoOeAHGLVj5elTEVMRGhXaxFClm+bbv52p2rDn3T5+apR60m2s/YOOdhNcfOxtC7vruFrogLYD5/Gsw9axEj05RwJb8MZQIDAQAB"
+  ]
+  ttl = 3600
+}
+
+
+resource "aws_route53_record" "google-workspace-spf" {
+  zone_id = aws_route53_zone.main.zone_id
+  name    = ""
+  type    = "SPF"
+  records = ["v=spf1 include:_spf.google.com ~all"]
+  ttl     = 3600
+}
+
+resource "aws_route53_record" "google-workspace-spf-txt" {
+  zone_id = aws_route53_zone.main.zone_id
+  name    = ""
+  type    = "TXT"
+  records = ["v=spf1 include:_spf.google.com ~all"]
+  ttl     = 3600
 }
