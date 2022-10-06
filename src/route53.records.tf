93ac516529b540abb050e9d2d208877c428be4a8
Nathan Hall <nathan@whatshark.com>
2022-10-06 22:04:42 +0100

    Set roots

--- src/route53.records.tf
+++ src/route53.records.tf
@@ -1,25 +1,34 @@
-resource "aws_route53_record" "github-challenge-mukvoting" {
-  zone_id = aws_route53_zone.main.zone_id
-  name    = "_github-challenge-mukvoting-org"
-  type    = "TXT"
-  ttl     = 3600
-  records = ["fc368a3d88"]
-}
-
-resource "aws_route53_record" "keybase" {
+resource "aws_route53_record" "txt-root" {
   zone_id = aws_route53_zone.main.zone_id
   name    = "whatshark.com"
   type    = "TXT"
   ttl     = 3600
-  records = ["keybase-site-verification=0-xAbh86MC_fqONGB5gStg6Z59JChhATmwgNEC4VS6"]
+  records = [
+    "keybase-site-verification=0-xAbh86MC_fqONGB5gStg6Z59JChhATmwgNEC4VS6",
+    "v=spf1 include:_spf.google.com ~all"
+  ]
 }
 
-resource "aws_route53_record" "github-pages-whatshark" {
+resource "aws_route53_record" "mx-root" {
   zone_id = aws_route53_zone.main.zone_id
-  name    = "_github-pages-challenge-whatshark"
-  type    = "TXT"
+  name    = "whatshark.com"
+  type    = "MX"
+  records = [
+    "1 aspmx.l.google.com",
+    "5 alt1.aspmx.l.google.com",
+    "5 alt2.aspmx.l.google.com",
+    "10 alt3.aspmx.l.google.com",
+    "10 alt4.aspmx.l.google.com"
+  ]
+  ttl = 3600
+}
+
+resource "aws_route53_record" "spf-root" {
+  zone_id = aws_route53_zone.main.zone_id
+  name    = "whatshark.com"
+  type    = "SPF"
+  records = ["v=spf1 include:_spf.google.com ~all"]
   ttl     = 3600
-  records = ["73f6c2e828fd4ad858453addec73af"]
 }
 
 resource "aws_route53_record" "google-workspace-redirect" {
@@ -30,20 +39,6 @@ resource "aws_route53_record" "google-workspace-redirect" {
   records = ["ghs.googlehosted.com"]
   ttl     = "3600"
 
-}
-
-resource "aws_route53_record" "google-workspace" {
-  zone_id = aws_route53_zone.main.zone_id
-  name    = ""
-  type    = "MX"
-  records = [
-    "1 aspmx.l.google.com",
-    "5 alt1.aspmx.l.google.com",
-    "5 alt2.aspmx.l.google.com",
-    "10 alt3.aspmx.l.google.com",
-    "10 alt4.aspmx.l.google.com"
-  ]
-  ttl = 3600
 }
 
 resource "aws_route53_record" "google-workspace-dkim" {
@@ -57,19 +52,18 @@ resource "aws_route53_record" "google-workspace-dkim" {
   ttl = 3600
 }
 
-
-resource "aws_route53_record" "google-workspace-spf" {
+resource "aws_route53_record" "github-challenge-mukvoting" {
   zone_id = aws_route53_zone.main.zone_id
-  name    = ""
-  type    = "SPF"
-  records = ["v=spf1 include:_spf.google.com ~all"]
+  name    = "_github-challenge-mukvoting-org"
+  type    = "TXT"
   ttl     = 3600
+  records = ["fc368a3d88"]
 }
 
-resource "aws_route53_record" "google-workspace-spf-txt" {
+resource "aws_route53_record" "github-pages-whatshark" {
   zone_id = aws_route53_zone.main.zone_id
-  name    = ""
+  name    = "_github-pages-challenge-whatshark"
   type    = "TXT"
-  records = ["v=spf1 include:_spf.google.com ~all"]
   ttl     = 3600
+  records = ["73f6c2e828fd4ad858453addec73af"]
 }
