a4aefe6e983262bcd3d37e9c78705e3bbab99dab
Nathan Hall <nathan@whatshark.com>
2022-10-07 12:36:50 +0100

    Move dnssec policy into route53

--- src/route53.tf
+++ src/route53.tf
@@ -1,3 +1,58 @@
+data "aws_iam_policy_document" "route53-dnssec-policy" {
+  version = "2012-10-17"
+  statement {
+    sid       = "Enable IAM User Permissions"
+    effect    = "Allow"
+    actions   = ["kms:*"]
+    resources = ["*"]
+    principals {
+      type        = "AWS"
+      identifiers = ["arn:aws:iam::${data.aws_caller_identity.current.account_id}:root"]
+    }
+  }
+
+  statement {
+    sid    = "Allow Route 53 DNSSEC Service"
+    effect = "Allow"
+    actions = [
+      "kms:DescribeKey",
+      "kms:GetPublicKey",
+      "kms:Sign"
+    ]
+    resources = ["*"]
+    condition {
+      test     = "StringEquals"
+      variable = "aws:SourceAccount"
+      values   = ["${data.aws_caller_identity.current.account_id}"]
+    }
+    condition {
+      test     = "ArnLike"
+      variable = "aws:SourceArn"
+      values   = ["arn:aws:route53:::hostedzone/*"]
+    }
+    principals {
+      type        = "Service"
+      identifiers = ["dnssec-route53.amazonaws.com"]
+    }
+  }
+
+  statement {
+    sid       = "Allow Route 53 DNSSEC to CreateGrant"
+    effect    = "Allow"
+    actions   = ["kms:CreateGrant"]
+    resources = ["*"]
+    principals {
+      type        = "Service"
+      identifiers = ["dnssec-route53.amazonaws.com"]
+    }
+    condition {
+      test     = "Bool"
+      variable = "kms:GrantIsForAWSResource"
+      values   = [true]
+    }
+  }
+}
+
 resource "aws_kms_key" "main-dnssec-key" {
   provider                 = aws.us-east-1
   description              = "Used for signing and verifying DNSSEC requests for whatshark.com"
